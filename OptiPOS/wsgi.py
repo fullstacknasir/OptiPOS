8b4426302bb568cca3bdd0b84d537edd7016299d
Mohammad Nasir <nasirkhancse@gmail.com>
2025-01-23 00:03:25 +0600

    rename the local_erp to OptiPOS

--- OptiPOS/wsgi.py
+++ OptiPOS/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for local_erp project.
+WSGI config for OptiPOS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'local_erp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OptiPOS.settings')
 
 application = get_wsgi_application()
